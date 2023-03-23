f68224163c80851298e7ea6e770c672e50c6f6c6
awadh30303 <awadh3030@hotmail.com>
2023-03-23 11:02:30 +0400

    Api project

---
+++ src/Api/MyObject.java
@@ -0,0 +1,14 @@
+package Api;
+
+import java.util.List;
+
+public class MyObject {
+	
+	
+	List<String> domains [];
+	String country;
+	List<String> web_pages [];
+	String name;
+	String alpha_two_code;
+
+}
